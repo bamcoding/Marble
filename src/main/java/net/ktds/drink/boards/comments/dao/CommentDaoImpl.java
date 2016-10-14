4e0b23b1f1978af958e8fa2d8038b08459b08ee3
206-026 <206-026@206-026>
2016-10-14 11:21:43 +0900

    dd

---
+++ src/main/java/net/ktds/drink/boards/comments/dao/CommentDaoImpl.java
@@ -0,0 +1,7 @@
+package net.ktds.drink.boards.comments.dao;
+
+import net.ktds.drink.support.DaoSupport;
+
+public class CommentDaoImpl extends DaoSupport implements CommentDao{
+
+}
---
+++ src/main/java/net/ktds/drink/boards/comments/vo/CommentVO.java
@@ -0,0 +1,56 @@
+package net.ktds.drink.boards.comments.vo;
+
+public class CommentVO {
+
+	private String commentId;
+	private String boardId;
+	private String commentContent;
+	private String userId;
+	private String createdDate;
+	private String modifyDate;
+	private String parentCommentId;
+	
+	public String getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(String commentId) {
+		this.commentId = commentId;
+	}
+	public String getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(String boardId) {
+		this.boardId = boardId;
+	}
+	public String getCommentContent() {
+		return commentContent;
+	}
+	public void setCommentContent(String commentContent) {
+		this.commentContent = commentContent;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getModifyDate() {
+		return modifyDate;
+	}
+	public void setModifyDate(String modifyDate) {
+		this.modifyDate = modifyDate;
+	}
+	public String getParentCommentId() {
+		return parentCommentId;
+	}
+	public void setParentCommentId(String parentCommentId) {
+		this.parentCommentId = parentCommentId;
+	}
+	
+}
