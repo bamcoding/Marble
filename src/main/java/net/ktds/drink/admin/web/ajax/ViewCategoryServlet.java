c688a89ff7c27eec67b15cd256be5e9d68091fd8
이근재 <hanzen927@gmail.com>
2016-10-17 20:32:18 +0900

    1

--- src/main/java/net/ktds/drink/admin/web/ajax/ViewCategoryServlet.java
+++ src/main/java/net/ktds/drink/admin/web/ajax/ViewCategoryServlet.java
@@ -1,6 +1,7 @@
 package net.ktds.drink.admin.web.ajax;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -9,14 +10,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.ktds.drink.games.biz.GamesBiz;
+import net.ktds.drink.games.biz.GamesBizImpl;
 import net.ktds.drink.games.vo.CategoryVO;
 
 public class ViewCategoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private List<CategoryVO> categories;
     private CategoryVO categoryVO;
+    private GamesBiz biz = new GamesBizImpl();
     public ViewCategoryServlet() {
-        super();
+    	super();
+    	categories = new ArrayList<CategoryVO>();
+    	categoryVO =  new CategoryVO();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
