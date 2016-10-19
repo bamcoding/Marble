f71e38c8a102724a826cdb970ead17a92e37a918
Yoonyujin <yyj624@naver.com>
2016-10-19 14:46:04 +0900

    no message

--- src/main/java/net/ktds/drink/constants/Session.java
+++ src/main/java/net/ktds/drink/constants/Session.java
@@ -6,12 +6,11 @@ public interface Session {
 
 	public static final String SEARCH_INFO = "_SEARCH_INFO_";
 	
-<<<<<<< HEAD
+
 	public static final String SEARCH_GAME_INFO = "_SEARCH_GAME_INFO_";
 	public static final String SEARCH_CUSTOM_INFO = "_SEARCH_CUSTOM_INFO_";
-=======
 	public static final String SEARCH_USER_INFO = "_SEARCH_USER_INFO_";
->>>>>>> 7b28a8be612f838318d3bfbbd7b58630267a547f
+
 	
 	public static final String ADMIN_INFO = "_ADMIN_INFO_";
 	
