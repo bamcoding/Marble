227f918459c18809a52b217dbe939f8cc3b5cb27
이근재 <hanzen927@gmail.com>
2016-10-21 11:32:45 +0900

    게임되는거에 트리됨

--- src/main/webapp/js/blueGrid.js
+++ src/main/webapp/js/blueGrid.js
@@ -2,23 +2,19 @@
 $('ul').each(function(){
   $this = $(this);
   $this.find("li").has("ul").addClass("hasSubmenu");
+  $this.closest('ul').css("border-left", "1px dashed #cccccc");
 });
 // Find the last li in each level
 $('li:last-child').each(function(){
   $this = $(this);
   // Check if LI has children
   if ($this.children('ul').length === 0){
+	// $this.closest('ul').css("border-left", "1px dashed #cccccc");
     // Add border-left in every UL where the last LI has not children
-    $this.closest('ul').css("border-left", "1px solid gray");
   } else {
-    // Add border in child LI, except in the last one
-    $this.closest('ul').children("li").not(":last").css("border-left","1px solid gray");
     // Add the class "addBorderBefore" to create the pseudo-element :defore in the last li
     $this.closest('ul').children("li").last().children("a").addClass("addBorderBefore");
     // Add margin in the first level of the list
-    $this.closest('ul').css("margin-top","20px");
-    // Add margin in other levels of the list
-    $this.closest('ul').find("li").children("ul").css("margin-top","20px");
   };
 });
 // Add bold in li and levels above
