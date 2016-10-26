9353a4b400b803448f0f99ccf4d8b3dfe1fdcf34
이근재 <hanzen927@gmail.com>
2016-10-26 13:35:14 +0900

    이미지 해봤다

--- src/main/java/net/ktds/drink/gameImages/biz/ImageBiz.java
+++ src/main/java/net/ktds/drink/gameImages/biz/ImageBiz.java
@@ -1,7 +1,12 @@
 package net.ktds.drink.gameImages.biz;
 
+import java.util.List;
+
 import net.ktds.drink.gameImages.vo.ImageVO;
 
 public interface ImageBiz {
+	public List<ImageVO> getAllImageList();
+	public boolean deleteImage(String imageId);
 	public boolean upLoadImage(ImageVO image);
+	
 }
